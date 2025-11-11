829f7fb31166216cf646c375bdb5b281fdbe65e3
bananaboy448 <zerobananaboy@gmail.com>
2025-11-11 21:59:46 +0100

    Primer commit

---
+++ src/psp/practica1/CaracterALlamada.java
@@ -0,0 +1,68 @@
+package psp.practica1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class CaracterALlamada {
+
+    public void transformar(String linea){
+
+        ProcessBuilder pb;
+        Process process;
+
+        if (!linea.isEmpty()){
+
+            String classname = "psp.practica1.NumRandom";
+            String currentPath = System.getProperty("user.dir");
+            String classpath = currentPath + "/out/production/PalabraNumRandom/";
+
+            try{
+
+                pb = new ProcessBuilder("java", "-cp", classpath, classname);
+                pb.inheritIO();
+
+                for (int i = 0; i < linea.length(); i++) {
+
+                    if ( !linea.toLowerCase().equals("fin")) {
+                        process = pb.start();
+                        process.waitFor();
+
+                        try (BufferedReader br = new BufferedReader(new FileReader(new File("temp.txt")))){
+                            String line;
+                            while ((line = br.readLine()) != null){
+                                System.out.print(line);
+                            }
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        CaracterALlamada caracterALlamada = new CaracterALlamada();
+        Scanner teclado = new Scanner(System.in);
+        String linea = "";
+
+        do{
+
+            System.out.println("\nIntroduce por teclado una linea y \"Fin\" para acabar:");
+            linea = teclado.nextLine();
+            caracterALlamada.transformar(linea);
+
+        } while(!(linea.toLowerCase().equals("fin")));
+
+    }
+
+}
---
+++ src/psp/practica1/NumRandom.java
@@ -0,0 +1,36 @@
+package psp.practica1;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Random;
+
+public class NumRandom {
+
+    public static void generarNum(){
+        Random random = new Random();
+        File archivo = new File("temp.txt");
+
+        try{
+            if(archivo.exists()){
+                archivo.delete();
+            }
+            archivo.createNewFile();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File("temp.txt")))){
+
+            bw.write(String.valueOf(random.nextInt(10)));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        generarNum();
+    }
+
+}
